bed007a1c0945d102e7eb1541fdef404f59f27b3
himanshu007-creator <addyjeridiq@gmail.com>
2022-01-29 12:36:59 +0530

    A2HS

--- js/script.js
+++ js/script.js
@@ -68,3 +68,31 @@ window.addEventListener("beforeinstallprompt", (e) => {
     });
   });
 });
+
+let deferredPromptsmsm;
+const addBtnsm = document.querySelector(".add-button-sm");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPromptsm = e;
+  // Update UI to notify the user they can add to home screen
+  addBtnsm.style.display = "block";
+
+  addBtnsm.addEventListener("click", (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtnsm.style.display = "none";
+    // Show the prompt
+    deferredPromptsm.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPromptsm.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the A2HS prompt");
+      } else {
+        console.log("User dismissed the A2HS prompt");
+      }
+      deferredPromptsm = null;
+    });
+  });
+});
